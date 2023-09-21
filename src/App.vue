b04ce819b64eb62dd5ef9b87ceb1735fbee41d14
francescomascellino <franesco.mascellino@gmail.com>
2023-09-21 14:19:20 +0200

    add options object and h1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue';
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+
+</script> -->
+
+<script>
+
+export default
+  {
+    data() {
+      return {
+        message: "Hello Vite"
+      }
+    },
+  }
+
+</script>
 
 <style scoped>
 .logo {
@@ -21,10 +24,16 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
